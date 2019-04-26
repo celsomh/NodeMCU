9a83a2f89b1033e44010f7fed7b96273bb71ea39
celsomh <c.martin01@ufromail.cl>
2019-04-26 11:21:33 -0400

    Envía mensaje a broker

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,88 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+
 // Creación de objeto que representa el sensor
-SoftwareSerial rfid = SoftwareSerial(12,   true);
+SoftwareSerial rfid = SoftwareSerial(12, true);
 
-boolean autentification;
-boolean hayLectura;
+bool autentification;
+bool hayLectura;
 
-const int led=14;
+const int led = 14;
 const int trigPin = 5;
 const int echoPin = 4;
 const int buzzer = 0;
 
-int maxDistance=20;
+const char *ssid = "DCI_1Piso";
+const char *password = "DCI.2019";
+const char *mqttServer = "broker.hivemq.com";
+const char *mqtt_topic = "dci/test";
+const int mqttPort = 1883; // MQTT PORT
+
+WiFiClient espClient;           // Creación de cliente WIFI
+PubSubClient client(espClient); // Creación de cliente MQTT
+
+int maxDistance = 20;
 long duration;
 int currentDistance;
 
-void setup()
+void setup_wifi()
 {
-  Serial.begin(9600);
-  rfid.begin(9600);
-  
-  autentification = false;
+  delay(100);
+  Serial.print("Conectando a: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("Coneccion WiFi realizada");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(led, OUTPUT);
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(6000);
+    }
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  int valor = 0;
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  String pwmValue_str = "";
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+    pwmValue_str = pwmValue_str + (char)payload[i];
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+  Serial.println(pwmValue_str);
 }
 
 void doSound()
@@ -38,12 +95,19 @@ void doSound()
 
 void lockSystem()
 {
-  if (!autentification){
+  if (!autentification)
+  {
     autentification = true;
-    Serial.println("Desbloqueado");
-  }else{
+    client.publish(mqtt_topic, "Desbloqueado");
+    client.subscribe(mqtt_topic);
+  
+  }
+  else
+  {
     autentification = false;
-    Serial.println("Bloqueado");
+    client.publish(mqtt_topic, "Bloqueado");
+    client.subscribe(mqtt_topic);
+    
   }
 }
 
@@ -55,16 +119,56 @@ void lockLed()
     digitalWrite(led, LOW);
 }
 
-void calcularDistancia(){
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2); 
-  
+void calcularDistancia()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); 
-  
+  delayMicroseconds(10);
+
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  currentDistance = (duration/2) / 29.1;
+  currentDistance = (duration / 2) / 29.1;
+}
+
+void setup()
+{
+  Serial.begin(115200); // baudios del dispositivo
+  rfid.begin(9600);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client"))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+  client.publish(mqtt_topic, "Succesfull");
+  client.subscribe(mqtt_topic);
+
+  autentification = false;
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop()
@@ -79,8 +183,14 @@ void loop()
 
   lockLed();
   calcularDistancia();
-  Serial.println(currentDistance);
   doSound();
   hayLectura = false;
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  //client.loop();
+
   delay(500);
 }
